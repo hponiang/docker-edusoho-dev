7855f5a7b8cabb3d34d8bed5c6494314b46d73a8
Simon <wq@zlzkj.com>
2016-12-16 01:16:44 +0800

    auto create ip/gateway/ssh port

--- docker-create-edusoho-dev.sh
+++ docker-create-edusoho-dev.sh
@@ -3,13 +3,91 @@
 ###############
 #!/bin/bash
 
-set -eo pipefail
+#check nginx
+# NGINX_SITE_ENABLE_DIR='/etc/nginx/sites-enabled'
 
-#read parameters
+# if [ ! -d "$NGINX_SITE_ENABLE_DIR" ]; then  
+#     echo >&2 "Error: $NGINX_SITE_ENABLE_DIR does not exsit, please check if the host has nginx installed or you can change the dir in this source code"
+#     exit 1
+# fi  
+
+# service nginx status
+# if [ $? -ne 0 ]; then
+#     echo >&2 'Error: service nginx status execute error, please check if the host has nginx installed' 
+#     exit 1
+# fi
+
+get_random_ssh_port(){
+    local min=10000
+    local max=20000
+    echo `expr $RANDOM % $(($max-$min)) + $min`
+}
+
+#specify a ssh port
+is_port_exsit=1
+while [ -n "$is_port_exsit" ]; do
+    ssh_port=$(get_random_ssh_port)
+    is_port_exsit=`lsof -i :${ssh_port} |grep "LISTEN" |awk '{print $1}'`
+done
+
+#input parameters
 read -p "input domain:" DOMAIN
-read -p "input docker container network name:" NETWORK
-read -p "input docker container ip:" IP
-read -p "input docker container ssh port:" SSH_PORT
+
+if [ -z "$DOMAIN" ]; then
+    echo >&2 'Error: please input a domain'
+    exit 1
+fi
+
+read -p "input edusoho-dev version (default: latest):" VERSION
+
+if [ -z "$VERSION" ]; then
+    VERSION='latest'
+fi
+
+#hardcode a network name
+NETWORK='esdev'
+
+get_random_ip_int(){
+    local ip_int=`expr $RANDOM % 254`
+    while [ $ip_int -eq 0 ]; do
+        ip_int=`expr $RANDOM % 254`
+    done
+    echo $ip_int
+}
+
+get_random_ip(){
+    local arr=(${1//./ })
+    local ip=${1}
+    for s in ${arr[@]}; do
+        if [ $s = 'x' ]; then
+            ip=${ip/x/$(get_random_ip_int)}
+        fi
+    done
+    echo $ip
+}
+
+get_gateway_segment(){
+    local arr=(${1//./ })
+    echo ${arr[0]}.${arr[1]}
+}
+
+is_network_exist=`docker network ls |grep ${NETWORK}`
+if [ -z "$is_network_exist" ]; then
+    for i in {50..0}; do
+        gateway=$(get_random_ip 172.x.0.1)
+        subnet=$(get_random_ip 172.x.0.0)
+        # todo check if gateway exsit
+        docker network create --gateway ${gateway} --subnet ${subnet}/16 esdev
+    done
+else
+    gateway=`docker network inspect ${NETWORK} |grep -e Gateway |awk  -F '\"' '{print $4}'`
+fi
+
+is_ip_exist=1
+while [ -n "$is_ip_exist" ]; do
+    ip=$(get_random_ip $(get_gateway_segment $gateway).x.x)
+    is_ip_exist=`docker network inspect ${NETWORK} |grep ${ip}`
+done
 
 #docker run
 mkdir -p /var/mysql/${DOMAIN} && \
@@ -17,20 +95,18 @@ rm -rf /var/mysql/${DOMAIN}/* && \
 docker run --name ${DOMAIN} -tid \
         -v /var/mysql/${DOMAIN}:/var/lib/mysql \
         -v /var/www/${DOMAIN}:/var/www/edusoho \
-        -p ${SSH_PORT}:22 \
-        --cpuset-cpus 2 \
-        --memory 2048m \
+        -p ${ssh_port}:22 \
         --network ${NETWORK} \
-        --ip ${IP} \
+        --ip ${ip} \
         -e DOMAIN="${DOMAIN}" \
-        -e IP="${IP}" \
-        edusoho/edusoho-dev
+        -e IP="${ip}" \
+        edusoho/edusoho-dev:${VERSION}
 
 #inject nginx config
 host='$host'
 remote_addr='$remote_addr'
 
-cat > /etc/nginx/sites-enabled/${DOMAIN} <<-EOF 
+cat > ${NGINX_SITE_ENABLE_DIR}/${DOMAIN} <<-EOF 
 server {
      listen 80;
      server_name ${DOMAIN};
@@ -45,5 +121,13 @@ server {
 }
 EOF
 
+echo '******************************************************'
+echo '1. change your root password:'
+echo " docker exec -ti ${DOMAIN} passwd root"
+echo '2. ssh login to your docker:'
+echo " ssh root@${DOMAIN} -p ${ssh_port}"
+echo '******************************************************'
 
-/etc/init.d/nginx reload
+echo 'nginx reloading'
+echo '*notice:if reloading failed, please execute "service nginx reload" yourself*'
+service nginx reload
